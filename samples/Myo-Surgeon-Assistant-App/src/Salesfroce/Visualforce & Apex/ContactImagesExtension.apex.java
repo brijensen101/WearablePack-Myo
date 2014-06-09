ff4eb44b9acee1681d94b6fc77a1ecca87fc1552
Ammar Alammar <ammar.alammar@gmail.com>
2014-06-09 19:23:26 +1000

    Update and rename ContactImagesExtension.apex.rtf to ContactImagesExtension.apex.java

---
+++ samples/Myo-Surgeon-Assistant-App/src/Salesfroce/Visualforce & Apex/ContactImagesExtension.apex.java
@@ -0,0 +1,30 @@
+public with sharing class ContactImagesExtension{
+ 
+    private ApexPages.standardController controller;
+    private Contact patient;
+    private Attachment photo;
+  
+    public ContactImagesExtension(ApexPages.StandardController controller) {
+        this.controller = controller;
+        this.patient = (Contact)controller.getRecord();
+    }
+ 
+    public Id patientPhotoId {
+        get {
+            if(photo == null){
+                try{
+                	this.photo = [SELECT Id, Name 
+                    	          FROM Attachment 
+                        	      WHERE ParentId = :patient.Id AND Name = 'ProfilePic'
+                            	  LIMIT 1][0];   
+                }
+                catch (Exception e){
+                    // In a production system, a thorough exception handling shoud be applied here.
+                    System.Debug(e.getMessage());
+                }
+                 
+            }
+            return this.photo.Id;
+        }
+    }
+}
